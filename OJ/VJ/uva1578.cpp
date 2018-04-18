0a840a3df196f5f84a121a3aa70297e6e419a772
andywu1998 <1078539713@qq.com>
2018-04-18 10:27:36 +0800

    4.18

--- OJ/VJ/uva1578.cpp
+++ OJ/VJ/uva1578.cpp
@@ -1,98 +1,22 @@
 #include <stdio.h>
 #include <iostream>
-#include <cstdlib>
-#include <cmath>
-#include <cctype>
-#include <string>
-#include <cstring>
 #include <algorithm>
-#include <stack>
-#include <queue>
-#include <set>
 #include <map>
-#include <ctime>
-#include <vector>
-#include <fstream>
-#include <list>
-#include <iomanip>
-#include <numeric>
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-#define ms(s) memset(s, 0, sizeof(s))
-const int inf = 0x3f3f3f3f;
-#define LOCAL
-
-
-struct rectangle
-{	
-	int w;
-	int h;
-};
-
+pair <int, int> a[6];
 int main(int argc, char * argv[]) 
 {
-    rectangle a[10];
-    while(cin >> a[0].w >> a[0].h)
-    {
-    	int cnt = 0;
-    	int marka, markb, markc;
-    	rectangle b[10];
-    	for (int i = 1; i < 6; i++)
-    	{
-    		cin >> a[i].w >> a[i].h;
-    	}
-
-    	for (int i = 0; i < 6; i++)
-    	{
-    		if (a[i].w > a[i].h)
-    		{
-    			swap(a[i].w, a[i].h);
-    		}
-    	}
-    	for (int i = 0; i < 6; i++)
-    	{
-    		for (int j = i + 1; j < 6; j++)
-    		{
-        	    if(a[i].w == a[j].w && a[i].h == a[j].h)
-    	    	{
-    				b[cnt].w = a[i].w;
-    				b[cnt++].h = a[i].h;
-     			}
-    		}
-    	}
-    	if (cnt != 3)
-    	{
-    		cout << "IMPOSSIBLE" << endl;
-    		continue;
-    	}
-
-    	for (int i = 0; i < 3; i++)
-    	{
-    		for (int j = i + 1; j < 3; j++)
-    		{
-    			if(b[i].w == b[j].w)
-    			{
-    				marka = i;
-    				markb = j;
-    			}
-    		}
-    	}
-    	for (int i = 0; i < 3; i++)
-    	{
-    		if (i != marka && i != markb)
-    		{
-    			markc = i;
-    		}
-    	}
-    	if((b[marka].h == b[markc].w && b[markb].h == b[markc].h) || (b[marka].h == b[markc].h && b[markb].h == b[markc].w))
-    	{
-    		cout << "POSSIBLE" << endl;
-    	}
-    	else
-    	{
-    		cout << "IMPOSSIBLE" << endl;
-    	}
+    while(cin >> a[0].first >> a[0].second)
+    { 
+        for (int i = 0; i < 6; i++)
+        {
+            if (i)
+                cin >> a[i].first >> a[i].second;
+            if(a[i].first > a[i].second) swap(a[i].first, a[i].second );
+        }
+        sort(a, a + 6);
+        puts(a[0].first == a[2].first && a[2].second == a[4].second && a[4].first == a[0].second 
+            && a[0] == a[1] && a[2] == a[3] && a[4] == a[5]?"POSSIBLE":"IMPOSSIBLE");
     }
     return 0;
 }
